b3b0fdc0fdcd2b0c94bd5baa22bcc9c3e9efb381
Metalkon <chrisc@bpgstudios.com>
2022-07-01 15:36:07 -0300

    fixed an the 10 errors but still won't assign player

--- Text Game Project/Program.cs
+++ Text Game Project/Program.cs
@@ -15,6 +15,7 @@ namespace Text_Game_Project
 
         static void Main()
         {
+            Fighter player = new Fighter();
             ConsoleHelper.SetCurrentFont("Consolas", 20); Console.Clear();
             Game myGame = new Game();
             myGame.Start();
@@ -22,7 +23,7 @@ namespace Text_Game_Project
 
 
             //test stuff
-            Console.WriteLine($"You have Chosen the {player.Name});
+            Console.WriteLine($"You have Chosen the {player.Name}");
             Console.WriteLine(player.Level);
             Console.WriteLine(player.Exp);
             Console.WriteLine(player.Health);
